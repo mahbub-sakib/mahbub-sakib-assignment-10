8b73cd61fc52d31143af4ccab61b31eb5dce2930
mahbub-sakib <mahbub.sakib418@gmail.com>
2022-09-11 11:52:46 +0600

    banner done

---
+++ src/pages/HomePage/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import banner from '../../../Images/banner.jpg';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner-container'>
+            <img className='banner-img' src={banner} alt="" />
+            <div className='banner-msg'>
+                <h1 className='general-h1'>Capture Your </h1>
+                <h1 className='general-h1'>Pawsome Moments</h1>
+                <h1 className='special-h1'>with Sakib</h1>
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;

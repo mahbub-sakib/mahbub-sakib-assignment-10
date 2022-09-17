eb258c391700a049670bf78332ef2c50a2df0e5f
mahbub-sakib <mahbub.sakib418@gmail.com>
2022-09-17 19:32:41 +0600

    user pass auth - login register done

--- src/pages/AuthPages/Login/Login.js
+++ src/pages/AuthPages/Login/Login.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (user) {
+        // navigate(from, { replace: true });
+        navigate('/home');
+        console.log(user);
+    }
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email, password);
+    }
+    const navigateRegister = event => {
+        navigate('/register');
+    }
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='register-form'>
+            <h2 className='p-2'>Login Page</h2>
+            <form onSubmit={handleLogin} className='form-styles'>
+                <input ref={emailRef} type="email" name="email" placeholder='Email Address' id="" required />
+                <input ref={passwordRef} type="password" name="password" placeholder='Password' id="" required />
+
+                <input className='w-50 mx-auto btn btn-primary mt-2' type="submit" value="Login" />
+            </form>
+            <p>New to this website? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
         </div>
     );
 };
--- src/pages/AuthPages/Register/Register.js
+++ src/pages/AuthPages/Register/Register.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import './Register.css';
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [passMisMatched, setPassMisMatch] = useState(true);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+    if (user) {
+        // navigate('/home');
+        console.log('user', user);
+    }
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target.confirmPassword.value;
+
+        if (password != confirmPassword) {
+            setPassMisMatch(false);
+        }
+        else {
+            // console.log(name, email, password, confirmPassword);
+
+            await createUserWithEmailAndPassword(email, password);
+
+            console.log('Updated profile', passMisMatched);
+            navigate('/home');
+        }
+
+    }
     return (
-        <div>
-            <h2>This is register</h2>
+        <div className='register-form'>
+            <h2 className='p-2'>Register Page</h2>
+            <form onSubmit={handleRegister} className='form-styles'>
+                <input type="text" name="name" id="" placeholder='Name' />
+
+                <input type="email" name="email" placeholder='Email Address' id="" required />
+
+                <input type="password" name="password" placeholder='Password' id="" required />
+                <input type="password" name="confirmPassword" placeholder='Confirm Password' id="" required />
+
+                {passMisMatched == false && <p className='text-danger'>Messages: Two password didn't match</p>}
+
+                <input className='w-50 mx-auto btn btn-primary mt-2' type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-primary pe-auto text-decoration-none' >Please Login</Link></p>
         </div>
     );
 };

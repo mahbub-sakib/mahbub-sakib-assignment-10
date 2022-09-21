f49ae1ee3f42ec5e49c8e0657a74be8f9a44f8dc
mahbub-sakib <mahbub.sakib418@gmail.com>
2022-09-21 10:18:26 +0600

    responsive menubar in progress

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -4,26 +4,34 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import './Header.css';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
 
 const Header = () => {
     const [user] = useAuthState(auth);
     const handleSignOut = () => {
         signOut(auth);
     }
+    const [open, setOpen] = useState(false);
+
     return (
         <div className='header-container'>
             <div>
+                <div className='d-lg-none'>
+                    <Link className='links ' to="/home"><img className='logo' src="./Images/logo.PNG" alt="" /></Link>
+                    <div onClick={() => setOpen(!open)} className={`my-3 menu-icon `}>
+                        {
+                            open ? <XIcon></XIcon> : <MenuIcon ></MenuIcon>
+                        }
+
+                    </div>
+                </div>
                 <ul className='nav-items'>
                     <Link className='links' to="/home">Home</Link>
                     <Link className='links' to="/blogs">Blogs</Link>
-
-                </ul>
-            </div>
-            <div>
-                <Link className='links' to="/home"><img className='logo' src="./Images/logo.PNG" alt="" /></Link>
-            </div>
-            <div>
-                <ul className='nav-items'>
+                    <div className='d-none d-md-block'>
+                        <Link className='links ' to="/home"><img className='logo' src="./Images/logo.PNG" alt="" /></Link>
+                    </div>
                     <Link className='links' to="/about">About Me</Link>
 
                     {
@@ -34,6 +42,7 @@ const Header = () => {
                     }
                 </ul>
             </div>
+
         </div>
     );
 };

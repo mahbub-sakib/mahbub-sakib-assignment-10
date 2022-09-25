dabd34fb19544c93b722e0288f3e248862d551f3
mahbub-sakib <mahbub.sakib418@gmail.com>
2022-09-25 09:39:36 +0600

    checkout page form and alert msg done

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import './Checkout.css';
 
 const Checkout = () => {
+    const emailRef = useRef('');
+    const userRef = useRef('');
+
+    const displayUserInfo = (event) => {
+        event.preventDefault();
+        const user = event.target.user.value;
+        const email = event.target.email.value;
+
+        alert('Thank you for the booking. Your name is ' + user + ' and your email is ' + email + '.');
+    }
+
     return (
-        <div>
-            <h2>this is checkout page</h2>
+        <div className='checkout-form'>
+            <h2>Checkout page</h2>
+
+            <form onSubmit={displayUserInfo} className='form-styles'>
+                <input ref={userRef} type="user" name="user" placeholder='User Name' id="" required />
+                <input ref={emailRef} type="email" name="email" placeholder='Email Address' id="" required />
+
+
+                <input className='w-50 mx-auto mt-2 butn-style' type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
